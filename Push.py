0fb008703c101cf47f8d8038d88b5265e3bf594b
Yue <62823952+hiyuey3@users.noreply.github.com>
2025-05-16 10:46:05 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Util.py

--- Push.py
+++
@@ -1,7 +0,0 @@
-from config import *
-import requests
-def sendMsg(msg):
-
-    PushAPI = f'https://push.xyw.cx/'
-    WCendPoint = f'/weixin.php'
-    requests.post('https://push.xyw.cx/weixin.php',data={'msg':msg,'openid':openid})
